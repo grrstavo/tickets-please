7673f1eb5e847503c80513292ede3f1b777afd47
Gustavo <gu1804@hotmail.com>
2025-07-15 21:15:04 -0300

    Implement API authentication with Sanctum and update routes for ticket management

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/tickets', function () {
-    return Ticket::all();
-});
